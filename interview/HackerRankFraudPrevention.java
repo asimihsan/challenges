590868decb4012ed0b07b12033346dbb11a9fa28
Asim Ihsan <asim.ihsan@youview.com>
2013-12-06 12:57:43 +0000

    Hacker Rank - Fraud Prevention - passes tests
    Added the post-processing of email addresses, street addresses, and
    states (by deferring to specific classes for each).

--- interview/HackerRankFraudPrevention.java
+++ interview/HackerRankFraudPrevention.java
@@ -6,17 +6,141 @@ import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+
+class EmailAddress {
+    private String original;
+    private String stripped;
+
+    EmailAddress(String original) {
+        this.original = original;
+        stripped = createStripped(original);
+    }
+
+    private static String createStripped(String original) {
+        StringBuilder output = new StringBuilder();
+        boolean startIgnoring = false;
+        boolean afterAtSign = false;
+        for (int i = 0; i < original.length(); i++) {
+            char c = original.charAt(i);
+            switch(c) {
+                case '.':
+                    if (!afterAtSign) continue;
+                    output.append(c);
+                    break;                    
+                case '+':
+                    startIgnoring = true;
+                    continue;
+                case '@':
+                    startIgnoring = false;
+                    afterAtSign = true;
+                    output.append(c);
+                    break;
+                default:
+                    if (startIgnoring) continue;
+                    output.append(c);
+                    break;
+            }
+        }
+        return output.toString();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("{original=%s, stripped=%s}", original, stripped);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof EmailAddress)) return false;
+        EmailAddress email = (EmailAddress)o;
+        return stripped.equals(email.stripped);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * stripped.hashCode();
+        return result;
+    }
+}
+
+abstract class MappedField {
+    private Map<String, String> mappings;
+    private String original;
+    private String mapped;    
+
+    MappedField(String original, Map<String, String> mappings) {
+        this.original = original;
+        this.mappings = mappings;
+        mapped = createMapped(original);
+    }
+
+    private String createMapped(String original) {
+        String output = original;
+        for (Map.Entry<String, String> entry : mappings.entrySet())
+            output = output.replaceAll(entry.getKey(), entry.getValue());
+        return output;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("{original=%s, mapped=%s}", original, mapped);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof MappedField)) return false;
+        MappedField m = (MappedField)o;
+        return mapped.equals(m.mapped);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + mapped.hashCode();
+        return result;
+    }
+}
+
+class StreetAddress extends MappedField {
+    static final private Map<String, String> mappings = new HashMap<>();
+    static {
+        mappings.put("street", "st.");
+        mappings.put("road", "rd.");
+    }
+
+    StreetAddress(String original) {
+        super(original, mappings);
+    }
+}
+
+class State extends MappedField {
+    static final private Map<String, String> mappings = new HashMap<>();
+    static {
+        mappings.put("illinois", "il");
+        mappings.put("california", "ca");
+        mappings.put("new york", "ny");
+    }
+
+    State(String original) {
+        super(original, mappings);
+    }
+}
 
 class Record implements Comparable<Record> {
     public String orderId;
     public String dealId;
-    public String emailAddress;
-    public String streetAddress;
+    public EmailAddress emailAddress;
+    public StreetAddress streetAddress;
     public String city;
-    public String state;
+    public State state;
     public String zipCode;
     public String creditCard; 
 
@@ -24,16 +148,21 @@ class Record implements Comparable<Record> {
         String[] elems = line.split(",");
         orderId = elems[0];
         dealId = elems[1];
-        emailAddress = elems[2];
-        streetAddress = elems[3];
+        emailAddress = new EmailAddress(elems[2]);
+        streetAddress = new StreetAddress(elems[3]);
         city = elems[4];
-        state = elems[5];
+        state = new State(elems[5]);
         zipCode = elems[6];
         creditCard = elems[7];
     }
 
     @Override
     public String toString() { return orderId; }
+
+    public String fullString() {
+        return String.format("{orderId=%s, dealId=%s, emailAddress=%s, streetAddress=%s, city=%s, state=%s, zipCode=%s, creditCard=%s}",
+                             orderId, dealId, emailAddress, streetAddress, city, state, zipCode, creditCard);
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -57,40 +186,45 @@ class Record implements Comparable<Record> {
 }
 
 class Rule {
-    String[] same;
-    String[] diff;
+    Field[] same;
+    Field[] diff;
     Rule(String[] same, String[] diff) {
-        this.same = same;
-        this.diff = diff;
+        try {
+            this.same = new Field[same.length];
+            for (int i = 0; i < same.length; i++)
+                this.same[i] = Record.class.getField(same[i]);
+            this.diff = new Field[diff.length];
+            for (int i = 0; i < diff.length; i++)
+                this.diff[i] = Record.class.getField(diff[i]);
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException("Constructor couldn't find field!", e);
+        }
     }
 
     public int hashCode(Record r) {
         int returnValue = 17;
         try {
-            for (String name : same) {
-                Field field = Record.class.getField(name);
+            for (Field field : same) {
                 returnValue = 31 * returnValue + field.get(r).hashCode();
             }
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            throw new RuntimeException("Rule couldn't find fields!", e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException("Rule couldn't access field!", e);
         }
         return returnValue;
     }
 
     public boolean areFraudulent(Record r1, Record r2) {
         try {
-            for (String name : same) {
-                Field field = Record.class.getField(name);
+            for (Field field : same) {
                 if (!( field.get(r1).equals(field.get(r2)) ))
                     return false;
             }
-            for (String name : diff) {
-                Field field = Record.class.getField(name);
+            for (Field field : diff) {
                 if ( field.get(r1).equals(field.get(r2)) )
                     return false;
             }
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            throw new RuntimeException("Rule couldn't find or access fields!", e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException("Rule couldn't access field!", e);
         }
         return true;
     }
@@ -103,7 +237,7 @@ class FraudDetector {
 
     FraudDetector(Rule rule, int totalRecords) {
         this.rule = rule;
-        Integer temp = totalRecords;
+        Integer temp = totalRecords / 5;
         BigInteger b = new BigInteger(temp.toString());
         size = Math.max(b.nextProbablePrime().intValue(), 11);
         lookup = new ArrayList<List<Record>>(size);
@@ -144,6 +278,7 @@ class HackerRankFraudPrevention {
                                             "dealId"},
                               new String[] {"creditCard"});
         FraudDetector f1, f2;
+        //List<Record> records = new ArrayList<>();
         try (
             InputStreamReader ir = new InputStreamReader(System.in);
             BufferedReader br = new BufferedReader(ir);
@@ -152,6 +287,7 @@ class HackerRankFraudPrevention {
             Integer number = null;
             if ((line = br.readLine()) != null)
                 number = new Integer(line);
+
             f1 = new FraudDetector(rule1, number);
             f2 = new FraudDetector(rule2, number);
             FraudDetector[] fs = {f1, f2};
@@ -160,9 +296,11 @@ class HackerRankFraudPrevention {
                 if (line.length() == 0)
                     continue;
                 Record record = new Record(line);
+                //records.add(record);
                 for (FraudDetector f : fs)
                     f.addRecord(record);
             }
+
             Set<Record> frauds = new HashSet<Record>();
             for (FraudDetector f : fs)
                 frauds.addAll(f.getFraudulentRecords());
@@ -173,8 +311,12 @@ class HackerRankFraudPrevention {
             for (int i = 0; i < result.size() - 1; i++)
                 System.out.print(String.format("%s,", result.get(i)));
             System.out.println(result.get(result.size() - 1));
+
         } catch (IOException e) {
             throw new RuntimeException("couldn't process file", e);
         }
+
+        //System.out.println(records.get(1580).fullString());
+        //System.out.println(records.get(9735).fullString());
     }
 }
