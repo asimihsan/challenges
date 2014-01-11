e4772b833e957f79a7f65b1f2b5966d5e3dc8ad7
Asim Ihsan <asim.ihsan@gmail.com>
2014-01-11 15:20:11 +0000

    emailaddress - handle double quoted strings in local part
    Either the entire local part or specific components of the local part
    may be double quoted. Once double quotes more character are permitted,
    including escaped backslashes and double quotes.
    
    Trying to get a single regular expression to work for the local part
    was making my eyes bleed, and moreover would become even more difficult
    with bracketed comments. Hence we move to using a hand-rolled parser,
    no doubt with an efficiency penalty.

--- codingforinterviews/emailaddress/lib/emailaddress.js
+++ codingforinterviews/emailaddress/lib/emailaddress.js
@@ -1,10 +1,99 @@
 emailAddress = (function() {
     'use strict';
+    var RE_PATTERN = /^(.+)@(.+)$/;
+    var RE_IN_DQUOTE_CHAR = /^(?:[A-Za-z0-9!#$%&\'*+\-\/=?_`{|}~^(),:;<>@\[\] \.]|\\["\\])$/;
+    var RE_OUT_DQUOTE_CHAR = /^(?:[A-Za-z0-9!#$%&\'*+\-\/=?_`{|}~^])$/;
+    var RE_DQUOTE = /"/;
+    var RE_DOT = /\./;
+    var STATE_START = 0,
+        STATE_IN_DQUOTE = 1,
+        STATE_OUT_DQUOTE = 2,
+        STATE_END_DQUOTE = 3;
+    var RE_DOMAIN = /^(?:[a-z0-9\-]+)(?:\.[a-z0-9\-]+)*$/;
 
-    var pattern = /^(?:[A-Za-z0-9!#$%&\'*+\-\/=?^_`{|}~]+\.?)+[^.]@(?:[a-z0-9\-]+(?!\.$)\.?)+$/;
+    var isValidLocalPart = function(string) {
+        var state = STATE_START;
+        //console.log('isValidLocalPart entry. string: ' + string);
+        for (var i = 0; i <= string.length; i++) {
+            var c;
+            if (i < string.length) {
+                c = string.charAt(i);
+            } else {
+                c = null;
+            }
+            //console.log('state: ' + state + ', c: ' + c);
+            switch (state) {
+
+            case STATE_START:
+                if (RE_DQUOTE.test(c)) {
+                    state = STATE_IN_DQUOTE;
+                } else if (RE_OUT_DQUOTE_CHAR.test(c)) {
+                    state = STATE_OUT_DQUOTE;
+                } else {
+                    return false;
+                }
+                break;
+
+            case STATE_IN_DQUOTE:
+                /**
+                 * We've assumed advancing by a single char is good
+                 * enough, but within double quotes backslashes and
+                 * double quotes must be escaped. RegExp lastIndex and
+                 * stick is Firefox-only, so instead just here be a
+                 * little cheeky and do it manually.
+                 */
+                var cs = string.slice(i, i+2);
+                if (RE_IN_DQUOTE_CHAR.test(cs)) {
+                    state = STATE_IN_DQUOTE;
+                    i += 1;
+                } else if (RE_IN_DQUOTE_CHAR.test(c)) {
+                    state = STATE_IN_DQUOTE;
+                } else if (RE_DQUOTE.test(c)) {
+                    state = STATE_END_DQUOTE;
+                } else {
+                    return false;
+                }
+                break;
+
+            case STATE_END_DQUOTE:
+                if (RE_DOT.test(c)) {
+                    state = STATE_START;
+                } else if (c === null) {
+                    return true;
+                } else {
+                    return false;
+                }
+                break;
+
+            case STATE_OUT_DQUOTE:
+                if (RE_OUT_DQUOTE_CHAR.test(c)) {
+                    state = STATE_OUT_DQUOTE;
+                } else if (RE_DOT.test(c)) {
+                    state = STATE_START;
+                } else if (c === null) {
+                    return true;
+                } else {
+                    return false;
+                }
+                break;
+            }
+        }
+        return false;
+    };
 
     var isValid = function(string) {
-        return pattern.test(string);
+        var match = RE_PATTERN.exec(string);
+        if (match === null) {
+            return false;
+        }
+        if (!isValidLocalPart(match[1])) {
+            return false;
+        }
+        var domain = match[2];
+        if (RE_DOMAIN.test(domain)) {
+            return true;
+        }
+        return false;
     };
 
     return {
--- codingforinterviews/emailaddress/test/emailaddress_test.js
+++ codingforinterviews/emailaddress/test/emailaddress_test.js
@@ -24,6 +24,34 @@ test('valid test 6', function() {
     ok(emailAddress.isValid('other.email-with-dash@example.com'));
 });
 
+test('valid test 7', function() {
+    ok(emailAddress.isValid('"much.more unusual"@example.com'));
+});
+
+test('valid test 8', function() {
+    ok(emailAddress.isValid('"very.unusual.@.unusual.com"@example.com'));
+});
+
+test('valid test 9', function() {
+    ok(emailAddress.isValid('"john".adams.doe@example.com'));
+});
+
+test('valid test 10', function() {
+    ok(emailAddress.isValid('john."adams".doe@example.com'));
+});
+
+test('valid test 11', function() {
+    ok(emailAddress.isValid('john.adams."doe"@example.com'));
+});
+
+test('valid test 12', function() {
+    ok(emailAddress.isValid('"john"."adams"."doe"@example.com'));
+});
+
+test('valid test 13', function() {
+    ok(emailAddress.isValid('"john\\".\\"adams\\".\\"doe"@example.com'));
+});
+
 test('invalid test 1', function() {
     ok(!emailAddress.isValid('Abc.example.com'));
 });
@@ -42,4 +70,40 @@ test('invalid test 4', function() {
 
 test('invalid test 5', function() {
     ok(!emailAddress.isValid('john..doe@host.com'));
+});
+
+test('invalid test 6', function() {
+    ok(!emailAddress.isValid('.user@host.com'));
+});
+
+test('invalid test 7', function() {
+    ok(!emailAddress.isValid('@host.com'));
+});
+
+test('invalid test 8', function() {
+    ok(!emailAddress.isValid('user@'));
+});
+
+test('invalid test 9', function() {
+    ok(!emailAddress.isValid('a"b(c)d,e:f;g<h>i[j\\k]l@example.com'));
+});
+
+test('invalid test 10', function() {
+    ok(!emailAddress.isValid('just"not"right@example.com'));
+});
+
+test('invalid test 11', function() {
+    ok(!emailAddress.isValid('this is"not\\allowed@example.com'));
+});
+
+test('invalid test 12', function() {
+    ok(!emailAddress.isValid('this\\ still\\"not\\\\allowed@example.com'));
+});
+
+test('invalid test 13', function() {
+    ok(!emailAddress.isValid('john.adams"."doe@example.com'));
+});
+
+test('invalid test 14', function() {
+    ok(!emailAddress.isValid('"john.adams."doe@example.com'));
 });
