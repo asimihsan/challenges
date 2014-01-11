9b6f02995948e63b873b02b6e503eb3412f2234b
Asim Ihsan <asim.ihsan@gmail.com>
2014-01-11 00:53:36 +0000

    emailaddress.js - linebreak style typo fix

--- codingforinterviews/emailaddress/test/emailaddress_test.js
+++ codingforinterviews/emailaddress/test/emailaddress_test.js
@@ -3,3 +3,23 @@
 test('valid test 1', function() {
     ok(emailAddress.isValid('niceandsimple@example.com'));
 });
+
+test('valid test 2', function() {
+    ok(emailAddress.isValid('very.common@example.com'));
+});
+
+test('valid test 3', function() {
+    ok(emailAddress.isValid('a.little.lengthy.but.fine@dept.example.com'));
+});
+
+test('valid test 4', function() {
+    ok(emailAddress.isValid('disposable.style.email.with+symbol@example.com'));
+});
+
+test('invalid test 1', function() {
+    ok(!emailAddress.isValid('Abc.example.com'));
+});
+
+test('invalid test 2', function() {
+    ok(!emailAddress.isValid('A@b@c@example.com'));
+});
